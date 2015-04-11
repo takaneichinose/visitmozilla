edc9b6193dba1ce5f3ddc6d6c24cc6991d0242f9
takaneichinose <ichinose.takane@gmail.com>
2015-04-11 15:16:25 +0800

    Change login link to button

--- js/views/index.js
+++ js/views/index.js
@@ -6,13 +6,17 @@ define([
 ], function($, _, Backbone, indexTemplate){
   var indexView = Backbone.View.extend({
     el: $('.row'),
-
     render: function(){
       compiledTemplate = _.template(indexTemplate);
       var data = {};
       this.$el.append(compiledTemplate, data);
+    },
+    events: {
+      'click #login': 'login'
+    },
+    login: function() {
+      console.log('bads');
     }
-    
   });
 
   return indexView;
